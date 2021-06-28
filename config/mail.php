ee0f12ebbfa12715071c9c0db80ca93ad71b6df3
AhmedK <ahmedmmkh@gmail.com>
2021-06-28 15:04:43 +0300

    email name

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'info@clouds-line.com'),
-        'name' => env('MAIL_FROM_NAME', 'Otantik'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@jana-sa.org'),
+        'name' => 'JANA Plus',
     ],
 
     /*
